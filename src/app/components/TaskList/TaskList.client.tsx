87f0e42c83fa60947f4942a109032526f3778835
Nicholas Engleman <nicholas.engleman@ajc.com>
2025-04-27 21:49:14 -0400

    fix type error

--- src/app/components/TaskList/TaskList.client.tsx
+++ src/app/components/TaskList/TaskList.client.tsx
@@ -1,9 +1,8 @@
-// components/TaskList.tsx
 "use client";
+
 import { Task } from "@prisma/client";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
 import Typography from "@mui/material/Typography";
 import TaskItem from "../TaskItem/TaskItem.client";
 
